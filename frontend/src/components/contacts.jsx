75d2e6b92216b43f1d3b637a42d11bb0690194fb
Ana Luiza de Freitas <132159788+analufreitasx@users.noreply.github.com>
2026-03-11 21:15:51 -0300

    Remove Whatsapp link from contacts component
    Removed Whatsapp contact link from the items array.

--- frontend/src/components/contacts.jsx
+++ frontend/src/components/contacts.jsx
@@ -14,7 +14,6 @@ const Contacts = () => {
         { href: 'https://linkedin.com/in/ana-luizadefreitas', label: 'Linkedin', icon: <FaLinkedin /> },
         { href: 'https://github.com/analufreitasx', label: 'Github', icon: <FaGithub /> },
         { href: 'mailto:analuizafreitas12@yahoo.com.br', label: 'Email', icon: <FaEnvelope /> },
-        { href: 'https://wa.me/5531992688681', label: 'Whatsapp', icon: <FaWhatsapp /> },
     ];
 
     return (
